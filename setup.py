d5721bf54d1da2d1fcb891efeff02d71ba82f330
Lane Shetron <laneshetron@gmail.com>
2019-04-03 19:22:46 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='zseries',
+    version='0.0.1',
+    url='https://github.com/laneshetron/zseries.git',
+    install_requires=requirements)
